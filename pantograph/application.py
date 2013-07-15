8b0490f8f3905d0c71e4f05ca68c847db79e2f12
Zhehao Mao <zhehao.mao@gmail.com>
2013-07-15 16:18:55 -0700

    add a custom tornado Application subclass

---
+++ pantograph/application.py
@@ -0,0 +1,27 @@
+import tornado.web
+import tornado.ioloop
+import json
+import os
+
+CONSTRUCTOR_SETTINGS = ["debug", "gzip", "cookie_secret", "login_url",
+                        "xsrf_cookies", "autoescape", "template_path", 
+                        "static_path", "static_url_prefix"]
+
+class PantographApplication(tornado.web.Application):
+    def __init__(self, handlers, **settings):
+        self.settings = settings
+        if os.path.isfile("./config.json"):
+            f = open("./config.json")
+            self.settings.update(json.load(f))
+
+        constr_args = {}
+        for key in CONSTRUCTOR_SETTINGS:
+            if key in self.settings:
+                constr_args[key] = self.settings[key]
+
+        tornado.web.Application.__init__(self, handlers, **constr_args)
+
+    def run(self, address = "127.0.0.1", port = 8080):
+        self.listen(port, address)
+        print("Pantograph now running at http://" + address + ":" + str(port))
+        tornado.ioloop.IOLoop.instance().start()
